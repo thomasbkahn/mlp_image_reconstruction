8654e86188c34b8afb859179b04027d24e35f770
tk <thomas.b.kahn@gmail.com>
2020-11-04 22:49:29 -0500

    improvements to reconstruct, now multiprocessing (not sure if it actually speeds up much though) and more automation

--- reconstruct.py
+++ reconstruct.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import multiprocessing
+from functools import partial
 
 import torch
 import numpy as np
@@ -46,6 +48,14 @@ def reconstruct_single(checkpoint, hallucination_buffer=None, scale_factor=None)
   return evaluate_model(checkpoint, n_x, n_y, x_lims, y_lims)
 
 
+def _reconstruct_all_worker(paths, hallucination_buffer=None, scale_factor=None):
+  checkpoint_path, output_path = paths
+  img = reconstruct_single(checkpoint_path, hallucination_buffer, scale_factor)
+  output_path.parent.mkdir(parents=True, exist_ok=True)
+  print(checkpoint_path, output_path)
+  cv2.imwrite(str(output_path), cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+
+# deprecated, use multiprocessing form and reconstuct_all
 def reconstruct_directory(checkpoint_dir, output_dir, hallucination_buffer=None,
                           glob_pattern="./*.pt"):
   checkpoint_dir = Path(checkpoint_dir)
@@ -57,20 +67,42 @@ def reconstruct_directory(checkpoint_dir, output_dir, hallucination_buffer=None,
     output_path = output_dir.joinpath(f"{checkpoint_path.stem}.png")
     cv2.imwrite(str(output_path), cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
 
+
+def reconstruct_all(checkpoint_toplevel, replace_source="reconstruction_checkpoints",
+                    replace_target="reconstruction_output", hallucination_buffer=None,
+                    glob_pattern="**/*.pt", n_procs=6):
+  checkpoint_toplevel = Path(checkpoint_toplevel)
+  checkpoint_paths = list(checkpoint_toplevel.glob(glob_pattern))
+  paths = []
+  for checkpoint_path in checkpoint_paths:
+    output_path_parts = [part.replace(replace_source, replace_target) for part in
+                         checkpoint_path.parts]
+    output_path = Path().joinpath(*output_path_parts).with_suffix(".png")
+    if output_path.exists():
+      continue
+    paths.append((checkpoint_path, output_path))
+  map_func = partial(_reconstruct_all_worker, hallucination_buffer=hallucination_buffer)
+  pool = multiprocessing.Pool(n_procs)
+  pool.map(map_func, paths)
+
+
+
+
 if __name__ == "__main__":
-  checkpoint_dirs = [
+  # checkpoint_dirs = [
     # "/home/tk/data/mlp_image_reconstruction_checkpoints/mabel_ithaca_experiment_big_run/mode_HSV",
     # "/home/tk/data/mlp_image_reconstruction_checkpoints/mabel_ithaca_experiment_big_run/mode_RGB",
     # "/shed/data/mlp_image_reconstruction_checkpoints/tom_cykana_pic/mode_RGB",
     # "/shed/data/mlp_image_reconstruction_checkpoints/tom_cykana_pic/mode_HSV",
-    "/shed/data/mlp_image_reconstruction_checkpoints/tom_cykana_pic_256x5",
-  ]
-  for checkpoint_dir in checkpoint_dirs:
-    checkpoint_dir = Path(checkpoint_dir)
-    output_dir_parts = []
-    for part in checkpoint_dir.parts:
-      output_dir_parts.append(part.replace("reconstruction_checkpoints", "reconstruction_output"))
+    # "/shed/data/mlp_image_reconstruction_checkpoints/tom_cykana_pic_256x5",
+  # ]
+  # for checkpoint_dir in checkpoint_dirs:
+    # checkpoint_dir = Path(checkpoint_dir)
+    # output_dir_parts = []
+    # for part in checkpoint_dir.parts:
+    #   output_dir_parts.append(part.replace("reconstruction_checkpoints", "reconstruction_output"))
 
-    output_dir = Path().joinpath(*output_dir_parts)
+    # output_dir = Path().joinpath(*output_dir_parts)
 
-    reconstruct_directory(checkpoint_dir, output_dir, hallucination_buffer=100)
+    # reconstruct_directory(checkpoint_dir, output_dir, hallucination_buffer=100)
+  reconstruct_all("/shed/data/mlp_image_reconstruction_checkpoints", hallucination_buffer=100)
